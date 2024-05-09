ac3956a7db159f9bdc3856ab4eda15a96f8d2bad
Oskar <o_swiderski@o2.pl>
2024-05-09 18:38:13 +0200

    zadanie zakonczone pomyslnie

--- app/src/main/java/com/example/trojmiastoapp/MainActivity.kt
+++ app/src/main/java/com/example/trojmiastoapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.trojmiastoapp
 
-
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.os.Bundle
 import android.widget.TextView
@@ -12,10 +11,18 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private val items = listOf(
-        Item(R.drawable.gdansk, "Gdańsk"),
-        Item(R.drawable.gdynia, "Gdynia"),
-        Item(R.drawable.sopot, "Sopot")
+    private val GDANSK_ID = R.id.gdansk_text_view
+    private val GDYNIA_ID = R.id.gdynia_text_view
+    private val SOPOT_ID = R.id.sopot_text_view
+    private val GDANSK_DRAWABLE = R.drawable.gdansk
+    private val GDYNIA_DRAWABLE = R.drawable.gdynia
+    private val SOPOT_DRAWABLE = R.drawable.sopot
+
+    private val items = mutableListOf(
+        Item(GDANSK_DRAWABLE, "Gdańsk"),
+        Item(GDYNIA_DRAWABLE, "Gdynia"),
+        Item(SOPOT_DRAWABLE, "Sopot")
+
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +39,51 @@ class MainActivity : AppCompatActivity() {
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.adapter = ItemsAdapter(items)
 
-        findViewById<TextView>(R.id.gdansk_text_view).setOnClickListener { showItems(0) }
-        findViewById<TextView>(R.id.gdynia_text_view).setOnClickListener { showItems(1) }
-        findViewById<TextView>(R.id.sopot_text_view).setOnClickListener { showItems(2) }
+        val textViews = listOf(
+            GDANSK_ID to "Gdańsk",
+            GDYNIA_ID to "Gdynia",
+            SOPOT_ID to "Sopot"
+        )
 
+        textViews.forEach { (id, title) ->
+            findViewById<TextView>(id).setOnClickListener { showItems(title) }
+        }
     }
-    private fun showItems(cityIndex: Int) {
-        val cityItems = items.filter { item -> item.title == items[cityIndex].title }
-        findViewById<RecyclerView>(R.id.recycler_view).adapter = ItemsAdapter(cityItems)
+
+    private fun showItems(cityTitle: String) {
+        items.clear()
+        val cityItems = items.filter { item -> item.title == cityTitle }
+        if (cityTitle == "Gdynia") {
+            items.addAll(
+                listOf(
+                    Item(GDYNIA_DRAWABLE, "Gdynia"),
+                    Item(R.drawable.gdynia_dar_pomorza, "Statek Dar Pomorza"),
+                    Item(R.drawable.gdynia_molo_orlowo, "Molo w Orłowie"),
+                    Item(R.drawable.gdynia_muzeum_emigracji, "Muzeum Emigracji")
+
+                )
+            )
+        }
+        if (cityTitle == "Sopot") {
+            items.addAll(
+                listOf(
+                    Item(SOPOT_DRAWABLE, "Sopot"),
+                    Item(R.drawable.sopot_molo, "Molo"),
+                    Item(R.drawable.sopot_ergo_arena, "Ergo Arena"),
+                    Item(R.drawable.sopot_opera, "Opera Leśna")
+                )
+            )
+        }
+        if (cityTitle == "Gdańsk") {
+            items.addAll(
+                listOf(
+                    Item(GDANSK_DRAWABLE, "Gdańsk"),
+                    Item(R.drawable.gdansk_bazylika, "Bazylika Mariacka Wniebowzięcia Najświętszej Maryi Panny"),
+                    Item(R.drawable.gdansk_park_oliwski, "Park Oliwski"),
+                    Item(R.drawable.gdansk_muzeum_2_wojny, "Muzeum 2 Wojny Światowej")
+                )
+            )
+        }
+        findViewById<RecyclerView>(R.id.recycler_view).adapter = ItemsAdapter(items)
     }
 }
